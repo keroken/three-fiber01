21bb8e093dccf9470c232d14fc38042227eba227
keroken <kenichi.shinagawa@gmail.com>
2021-05-22 22:11:11 +0900

    add interactivity: onClick, and add texture: random-letter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import './App.css';
+import randomLetter from './assets/random-letter.png';
 
 const BoxMotion = (props) => {
   useFrame(({ clock }) => {
@@ -22,20 +23,39 @@ const TorusMotion = (props) => {
 function App() {
   const boxMesh = React.useRef();
   const sphereMesh = React.useRef();
+  const [activeBox, setActiveBox] = useState(false);
+  const [activeTorus, setActiveTorus] = useState(false);
+  const texture = useMemo(() => new THREE.TextureLoader().load(randomLetter), []);
+
+  const Box = (props) => {
+    return (
+      <mesh ref={boxMesh} position={[2, 0, 0]} scale={activeBox ? [2, 2, 2] : [1, 1, 1]} onClick={() => setActiveBox(!activeBox)}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={"#ff00ff"}>
+          <primitive attach="map" object={texture} />
+        </meshStandardMaterial>
+      </mesh>
+    );
+  };
+
+  const Torus = (props) => {
+    return (
+      <mesh ref={sphereMesh} position={[-2, 0, 0]} scale={activeTorus ? [1.5, 1.5, 1.5] : [1, 1, 1]} onClick={() => setActiveTorus(!activeTorus)}>
+        <torusGeometry args={[1, 0.3, 8, 8]} />
+        <meshStandardMaterial color={"#a0ff00"} side={THREE.DoubleSide}>
+          <primitive attach="map" object={texture} />
+        </meshStandardMaterial>
+      </mesh>
+    );
+  };
 
   return (
     <div id="canvas-container">
       <Canvas camera={{ position: [0, 0, 7] }}>
         <ambientLight intensity={0.5}  />
         <pointLight position={[10, 10, 10]} />
-        <mesh ref={boxMesh} position={[2, 0, 0]}>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color={"#ff00ff"} />
-        </mesh>
-        <mesh ref={sphereMesh} position={[-2, 0, 0]}>
-          <torusGeometry args={[1, 0.3, 8, 8]} />
-          <meshStandardMaterial color={"#a0ff00"} side={THREE.DoubleSide} />
-        </mesh>
+        <Box />
+        <Torus />
         <BoxMotion meshRef={boxMesh} />
         <TorusMotion meshRef={sphereMesh} />
       </Canvas>
